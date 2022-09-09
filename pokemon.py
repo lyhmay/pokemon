7bae60bc468ccc923bbbf4ee315621738cb83982
lyh <415545136@qq.com>
2022-09-09 20:55:26 +0800

    增加了数据预处理

--- pokemon.py
+++ pokemon.py
@@ -3,6 +3,8 @@ import os,glob
 import  random,csv
 
 from torch.utils.data import Dataset
+from torchvision import  transforms
+from PIL import Image
 
 class Pokemon(Dataset):
     def __init__(self, root, resize, mode):
@@ -16,6 +18,18 @@ class Pokemon(Dataset):
             self.name2label[name] =len(self.name2label.keys())
         print(self.name2label)
         self.images,self.labels=self.load_csv('images.csv')
+
+        if mode=='train':
+            self.images = self.images[0:int(0.6*len(self.images))]
+            self.labels = self.labels[0:int(0.6*len(self.labels))]
+        elif mode == 'val':
+            self.images = self.images[int(0.6 * len(self.images)):int(0.8 * len(self.images))]
+            self.labels = self.labels[int(0.6 * len(self.labels)):int(0.8 * len(self.images))]
+        elif mode == 'test':
+            self.images = self.images[int(0.8* len(self.images)): len(self.images)]
+            self.labels = self.labels[int(0.8* len(self.labels)): len(self.images)]
+
+
     def load_csv(self, filename):
         if not os.path.exists(os.path.join(self.root, filename)):
             images = []
@@ -49,12 +63,27 @@ class Pokemon(Dataset):
         return images,labels
 
     def __len__(self):
-        pass
+        return  len(self.images)
 
     def __getitem__(self, idx):
-        pass
+
+        img,label =self.images[idx],self.labels[idx]
+        tf = transforms.Compose(
+            [
+                lambda x:Image.open(x).convert('RGB'),  # string path -> image data
+                transforms.Resize((self.resize,self.resize)),
+                transforms.ToTensor()
+            ]
+        )
+        img = tf(img)
+        label = torch.tensor(label)
+        return img,label
 
 def main():
+
     db  = Pokemon('./pokemon',224,'train')
+    x,y = next(iter(db))
+    print('sample:',x.shape,y.shape,y)
+
 if __name__ == '__main__':
     main()
